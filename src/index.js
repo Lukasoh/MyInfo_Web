66180957e2519167b912211980a66fe8faeb4f9c
Lukasoh <ekzmwor@gmail.com>
2024-03-21 04:41:43 +0800

    Updated js and css

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import profileImg from './images/profile.png';
+import profileImg from './assets/profile.png';
 
 function MainSc() 
 {
     return (
-        <div className="headerBg">
+        <div className="Main">
+            <div className="headerBg">
             <h2 className="titleTxt">
                 Developer Seonwoo Oh
             </h2>
-            <img src={profileImg} alt="Profile" className="my-img">
-            </img>
-        </div>        
+            <img src={profileImg} alt="Profile" className="my-img"/>
+            </div>    
+            <div className="email">Email: ekzmwor@gmail.com</div>
+            <div className="contactInfo">
+                <button className="github">GitHub: https://github.com/Lukasoh</button>
+            </div>
+            <div className="aboutMe">
+                <p>About Me</p>    
+            </div>            
+        </div>
+              
     );
 }
 
